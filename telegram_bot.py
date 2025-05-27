5662e52aa08a88fad0c8b0075431b63231a9fbd1
ben <cosmicdeveloper.b@gmail.com>
2025-05-27 19:50:38 +0100

    Improve the logic

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,10 +10,10 @@ from dotenv import load_dotenv
 import os
 import requests
 
-# LOad telegram data
 load_dotenv()
 TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
+logger = logging.getLogger(__name__)
 r = wait_for_redis()
 
 
@@ -28,51 +28,65 @@ def send_telegram_message(token, chat_id, message):
     requests.post(url, data=payload)
 
 
+def format_msg(name, new_signals):
+    lines = set()
+
+    if new_signals:
+        for value in new_signals.values():
+            logger.info(f'{value} is new')
+            lines.add(f'🦉 {value}')
+            r.sadd(name, value)
+
+    if lines:
+        message_body = '\n'.join(lines)
+        message = f"\n📌 *{name}*\n{message_body}"
+        send_telegram_message(TOKEN, CHAT_ID, message)
+
+    else:
+        logger.info('No new signals.')
+
+
 SIGNALS_KEY = 'signals_key'
 last_reset_time = time.time()
 
 
-RESET_INTERVAL_SECONDS = 72 * 60 * 60
+RESET_INTERVAL_SECONDS = 168 * 60 * 60
 
 
-def send_if_changed(name, new_data):
+def filter_data(name, new_data):
+    """
+    This function prevent repetitive signals
+    and update the redis key set
+    Reset all the signals every week
+    """
     global last_reset_time
     current_time = time.time()
-    lines = []
 
-    # Reset sent signals every RESET_INTERVAL_SECONDS (72 hours in this case)
     if current_time - last_reset_time > RESET_INTERVAL_SECONDS:
         r.delete(SIGNALS_KEY)
         r.delete(name)
         last_reset_time = current_time
-        logging.info('Signal memory cleared after 120 hours.')
+        logger.info('Signal memory cleared after a week.')
 
     if new_data is not None:
-        seen_signals = r.smembers(name)
+        sent_signals = r.smembers(SIGNALS_KEY)
+
+        new_signals = {}
+
         for signal_key, signal_value in new_data.items():
             signal_key_str = str(signal_key)
 
-            if not r.sismember(SIGNALS_KEY, signal_key_str):
-                if signal_value not in seen_signals:
-                    lines.append(f'🦉 {signal_value}')
-                    r.sadd(SIGNALS_KEY, signal_key_str)
-                    r.sadd(name, signal_value)
-                    logging.info("SIGNALS_KEY members: %s", r.smembers(SIGNALS_KEY))
-                    logging.info("%s members: %s", name, r.smembers(name))
-
-        if lines:
-            message_body = '\n'.join(lines)
-            message = f"\n📌 *{name}*\n{message_body}"
-            send_telegram_message(TOKEN, CHAT_ID, message)
-        else:
-            logging.info('No new signals.')
+            if signal_key_str not in sent_signals:
+                new_signals[signal_key_str] = signal_value
+                r.sadd(SIGNALS_KEY, signal_key_str)
+        format_msg(name, new_signals)
 
 
 def send_signals():
     while True:
         dataframe = get_all_rates()
-        send_if_changed('Divergence', find_divergence(dataframe))
-        send_if_changed('SMA Cross', sma_cross(dataframe))
-        send_if_changed('Fibonacci', get_fibo(dataframe))
-        send_if_changed('Ichimoku Cloud', ichimoku_signal(dataframe))
+        filter_data('Divergence', find_divergence(dataframe))
+        filter_data('SMA Cross', sma_cross(dataframe))
+        filter_data('Fibonacci', get_fibo(dataframe))
+        filter_data('Ichimoku Cloud', ichimoku_signal(dataframe))
         time.sleep(3600)
